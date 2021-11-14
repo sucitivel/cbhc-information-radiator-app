de7e7d052fdad74ae254e4396bdc9cdc75e681f9
Brad Zasada <bzasada@highlandsolutions.com>
2021-11-14 14:48:07 -0700

    - adds calendar

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,3 +6,22 @@ import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
+
+document.addEventListener('DOMContentLoaded', function() {
+  var calendarEl = document.getElementById('calendar');
+
+  var calendar = new Calendar(calendarEl, {
+    plugins: [ interactionPlugin, dayGridPlugin, timeGridPlugin, listPlugin ],
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+    },
+    initialDate: new Date(),
+    navLinks: true, // can click day/week names to navigate views
+    editable: true,
+    dayMaxEvents: true, // allow "more" link when too many events
+  });
+
+  calendar.render();
+})
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,45 +2,17 @@
 @section('content')
   <div class="py-10">
     <header>
-      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <h1 class="text-3xl font-bold leading-tight text-gray-900">
           Daily Checklist
         </h1>
       </div>
     </header>
     <main>
-      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
         <!-- Replace with your content -->
         <!-- This example requires Tailwind CSS v2.0+ -->
-        <div class="flow-root">
-          <ul role="list" class="-mb-8">
-
-            @foreach($tasks as $task)
-            <li>
-              <div class="relative pb-8">
-                <span class="absolute top-4 left-4 -ml-px h-full w-0.5 bg-gray-200" aria-hidden="true"></span>
-                <div class="relative flex space-x-3">
-                  <div>
-                    <span class="h-8 w-8 rounded-full bg-green-500 flex items-center justify-center ring-8 ring-white">
-                      <!-- Heroicon name: solid/check -->
-                      <svg class="h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                        <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
-                      </svg>
-                    </span>
-                  </div>
-                  <div class="min-w-0 flex-1 pt-1.5 flex justify-between space-x-4">
-                    <div>
-                      <p class="text-sm text-gray-500">{{ $task[0] }}</p>
-                    </div>
-                    <div class="text-right text-sm whitespace-nowrap text-gray-500">
-                      <time datetime="2020-09-28">Sep 28</time>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </li>
-            @endforeach
-          </ul>
+        <div id="calendar">
         </div>
 
 
