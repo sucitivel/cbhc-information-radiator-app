17d270258406f0db205d78f6b39679cb440a3eff
Brad Zasada <bzasada@highlandsolutions.com>
2021-11-14 15:39:23 -0700

    - arudd

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,7 @@ import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
+import interactionPlugin from '@fullcalendar/interaction';
 
 document.addEventListener('DOMContentLoaded', function() {
   var calendarEl = document.getElementById('calendar');
